57d3c3919b20b8b45f5d206cdfbdf8b9f864dd89
Kevin Elliott <kevin@welikeinc.com>
2019-02-13 00:17:43 -0800

    Hopefully this is the correct definition of the func.

--- src/acars.c
+++ src/acars.c
@@ -117,7 +117,7 @@ static void output_acars_json(la_proto_node const * const node) {
 	return vstr;
 }
 
-static void format_acars_json(la_proto_node const * const node) {
+la_vstring *format_acars_json(la_proto_node const * const node) {
 	if(node == NULL || node->td != &la_DEF_acars_message) {
 		return;
 	}
@@ -128,8 +128,11 @@ static void format_acars_json(la_proto_node const * const node) {
 			*ptr = ' ';
 		}
 	}
-	fprintf(outf, "{ 'mode': '%1c', 'reg': '%7s', 'ack': '%1c', 'label': '%2s', 'blockId': '%1c', 'no': '%4s', 'flight_id': '%6s', txt: '%s' }",
+	la_vstring *vstr = la_vstring_new();
+	la_vstring_append_sprintf(vstr,
+	  "{ 'mode': '%1c', 'reg': '%7s', 'ack': '%1c', 'label': '%2s', 'blockId': '%1c', 'no': '%4s', 'flight_id': '%6s', txt: '%s' }",
 		msg->mode, msg->reg, msg->ack, msg->label, msg->block_id, msg->no, msg->flight_id, txt);
+	return vstr;
 }
 
 void output_acars(void const *msg) {
